20a8f77f0bfdeb40faa4d54c7d9b61379c9add9b
R-AS <xdn2115@126.com>
2019-09-24 11:39:33 +0800

    add 2019.9.14

---
+++ JS/2019.9.14/1/index.js
@@ -0,0 +1,10 @@
+/**
+ * 封装函数 f，使 f 的 this 指向指定的对象
+ */
+
+function bindThis(f, oTarget) {
+  if (f.bind) return f.bind(oTarget);
+  return function () {
+    return f.apply(oTarget, arguments);
+  }
+} 
